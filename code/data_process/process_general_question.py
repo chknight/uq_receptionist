46d7f96418bf93bbb675c6162ba89d6b4b84156d
chknight <ch.knight05@gmail.com>
2017-05-08 18:43:22 +1000

    update question keyword extraction

---
+++ code/data_process/process_general_question.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import os, os.path, codecs
+import json
+import csv
+import operator
+import MySQLdb
+from nltk.corpus import stopwords
+
+conn = MySQLdb.connect(host= "localhost",
+                  user="root",
+                  passwd="19941005",
+                  db="uq_receptionist")
+cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+
+
+# fetch general question
+def fetch_data_from_database():
+    cursor.execute('''SELECT * FROM general_question''')
+    result = cursor.fetchall()
+    print(result)
+    return result
+
+data = fetch_data_from_database()
+
+for row in data:
+    text = row['question'].lower()
+    text = text.replace("?", "")
+    text = text.replace("uq", "")
+    text = text.replace('"', "")
+    text = text.replace('(', "")
+    text = text.replace(')', '')
+    text = text.replace("what's", '')
+    text = text.replace("'s", '')
+    words = text.split(" ")
+    filtered_words = [word for word in words if word not in stopwords.words('english')]
+    insert = ""
+    for filtered in filtered_words:
+        if (filtered != ""):
+            insert = insert + filtered + ","
+    print(insert)
+    cursor.execute("""
+       UPDATE general_question
+       SET keyword=%s
+       WHERE id=%s
+        """, [insert, row['id']])
+conn.commit()
+conn.close()
